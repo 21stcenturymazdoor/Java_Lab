7f0831615f21a01a10d09d675b1928b890c159c3
21stcenturymazdoor <21stcenturymazdoor@gmail.com>
2025-06-15 19:38:55 +0530

    Linked List made Object Oriented

--- Lab7/Linked_List_Object_Oriented/Node.java
+++ Lab7/Linked_List_Object_Oriented/Node.java
@@ -2,6 +2,10 @@ package Linked_List_Object_Oriented;
 /**
  * The Node class represents a node in the SLL.
  *
+ * To showcase inter-object communication between Node and SLL
+ * Node object has the methods instead of SLL
+ * Node recursively calls the method
+ *
  * @author (21stcenturymazdoor)
  * @version (15/06/2025)
  */
@@ -23,5 +27,40 @@ public class Node
     
     void setNext(Node n){
         this.next = n;
+    }
+    
+    void insertAtEnd(int d){
+        if(next != null){
+            next.insertAtEnd(d);
+            return;
+        }
+        
+        this.next = new Node(d,null);
+    }
+    
+    Node deleteByValue(int value) {
+        if (this.next != null) {
+            this.next = this.next.deleteByValue(value);
+        }
+        if (this.data == value) {
+            return this.next;
+        } else {
+            return this;
+        }
+    }
+    
+    int getSum(){
+        if(next != null){
+            return this.data + next.getSum();
+        }
+        
+        return data;
+    }
+    
+    void display(){
+        System.out.print(data + " --> ");
+        if(next != null){
+            next.display();
+        }
     }
 }
--- Lab7/Linked_List_Object_Oriented/SLL.java
+++ Lab7/Linked_List_Object_Oriented/SLL.java
@@ -2,6 +2,9 @@ package Linked_List_Object_Oriented;
 /**
  * Implementation of Singly linked list
  *
+ * To showcase inter-object communication between Node and SLL
+ * Node object has the methods instead of SLL
+ *
  * @author (21stcenturymazdoor)
  * @version (14/06/2025)
  */
@@ -15,66 +18,39 @@ public class SLL
     }
     
     public void insertAtBegin(int d){
-        Node n = new Node(d,head);
-        head = n;
+        head = new Node(d, head);
     }
     
     public int deleteAtBegin(){
         if (head == null) {
             return -999;
         }
+        
+        int val = head.data;
+        
         Node temp = head;
         head = head.next;
-
-        int tempData = temp.data;
         temp = null;
         
-        return tempData;
+        return val;
     }
     
     public void insertAtEnd(int d){
-        Node newNode = new Node(d, null);
         if (head == null) {
-            head = newNode;
+            head = new Node(d, null);
         } else {
-            Node temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
+            head.insertAtEnd(d);
         }
     }
     
-    public void deleteByValue(int d){
-        Node curr = head;
-        Node prev = null;
-
-        while (curr != null) {
-            if (curr.data == d) {
-                if (prev == null) {
-                    // Deleting head node
-                    head = curr.next;
-                } else {
-                    prev.next = curr.next;
-                }
-
-                //freeing up the node
-                Node toDelete = curr;
-                curr = curr.next;
-                toDelete = null;
-            } else {
-                prev = curr;
-                curr = curr.next;
-            }
-        }
+    public void deleteByValue(int value){
+        head = (head != null) ? head.deleteByValue(value) : null ;
     }
     
     public int getSum(){
-        Node temp = head;
         int sum = 0;
-        while(temp != null){
-            sum += temp.data;
-            temp = temp.next;
+        if(head != null){
+            sum += head.getSum();
         }
         
         return sum;
@@ -82,10 +58,8 @@ public class SLL
     
     public void display(){
         System.out.print("Head --> ");
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " --> ");
-            temp = temp.next;
+        if(head != null){
+            head.display();
         }
         System.out.println("null");
     }
